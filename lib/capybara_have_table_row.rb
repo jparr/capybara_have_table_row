f6f952265acd242d29529c69b1b5782873b81afd
Jeff Parr <jeffparr100@gmail.com>
2023-07-13 10:23:55 -0700

    bundle gem new

---
+++ lib/capybara_have_table_row.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "capybara_have_table_row/version"
+
+module CapybaraHaveTableRow
+  class Error < StandardError; end
+  # Your code goes here...
+end
